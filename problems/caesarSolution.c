57387314c7f47276d11198b1b2b442f25cc3e8dc
Mohammed Safwan <mohammedsafwan261205@gmail.com>
2025-11-29 14:06:59 +0530

    solutions to problems

---
+++ problems/caesarSolution.c
@@ -0,0 +1,44 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc == 2)
+    {
+        for (int l = 0; l < strlen(argv[1]); l++)
+        {
+            if (isalpha(argv[1][l]))
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    int key = atoi(argv[1]);
+
+    char text[100];
+    printf("plaintext: ");
+    fgets(text, sizeof(text), stdin);
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            if (isupper(text[i]))
+            {
+                text[i] = (((text[i] - 65) + key) % 26) + 65;
+            }
+            if (islower(text[i]))
+            {
+                text[i] = (((text[i] - 97) + key) % 26) + 97;
+            }
+        }
+    }
+    printf("ciphertext: %s\n", text);
+}
